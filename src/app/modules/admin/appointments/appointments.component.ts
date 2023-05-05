631280ce0a03e02a9d0adcb97b0386c4e6861db9
Jamel Eid Yassin <42769414+jamelyassin84@users.noreply.github.com>
2023-05-05 07:13:14 +0400

    Changes

--- src/app/modules/admin/appointments/appointments.component.ts
+++ src/app/modules/admin/appointments/appointments.component.ts
@@ -2,7 +2,9 @@ import {Component} from '@angular/core'
 import {dbwAnimations} from '@digital_brand_work/animations/animation.api'
 import {AppointmentTypeEnum} from 'app/app-core/enums/appointment-type.enum'
 import {RHUEnum} from 'app/app-core/enums/rhu.enum'
-import { AddAppointmentModal } from 'app/modules/modals/add-appointment-modal/add-appointment-modal.service'
+import {AddAppointmentModal} from 'app/modules/modals/add-appointment-modal/add-appointment-modal.service'
+import * as XLSX from 'xlsx'
+import * as FileSaver from 'file-saver'
 
 @Component({
     selector: 'appointments',
@@ -12,7 +14,7 @@ import { AddAppointmentModal } from 'app/modules/modals/add-appointment-modal/ad
 })
 export class AppointmentsComponent {
     constructor(private _addAppointmentModal: AddAppointmentModal) {}
-    
+
     readonly addAppointmentModalOpened$ = this._addAppointmentModal.opened$
 
     readonly RHU = Object.values(RHUEnum)
@@ -21,4 +23,36 @@ export class AppointmentsComponent {
 
     currentRHU = this.RHU[0]
     currentService = this.SERVICES[0]
+
+    generateExcel() {
+        const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(
+            [1, 2, 3, 4].map(() => {
+                return {
+                    id: 'JD34MN4BHGDKH',
+                    Patient: 'John Doe',
+                    'Appointment Type': 'General',
+                    'Nature of Visit': 'New Admission',
+                    Symptoms: 'Dry and Blood in Cough',
+                    Diagnosis: 'Tuberculosis',
+                    'Medication/Treatment': 'Anti-biotic',
+                    Provider: 'Mary Jane',
+                    'Findings/Impression': 'Phlegm in upper chest',
+                    'Laboratory Test': 'X-ray',
+                }
+            }),
+        )
+
+        const workbook: XLSX.WorkBook = {
+            Sheets: {data: worksheet},
+            SheetNames: ['data'],
+        }
+        const excelBuffer: any = XLSX.write(workbook, {
+            bookType: 'xlsx',
+            type: 'array',
+        })
+        const data: Blob = new Blob([excelBuffer], {
+            type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8',
+        })
+        FileSaver.saveAs(data, `generated-${Date.now().toString()}.xlsx`)
+    }
 }
